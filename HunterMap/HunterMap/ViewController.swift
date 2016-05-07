965594bcf17109f5562a972359037a246ba818f3
佐橘　一旗 <01002552@CA1100.local>
2016-05-07 15:12:05 +0900

    initial commit

---
+++ HunterMap/HunterMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HunterMap
+//
+//  Created by 佐橘　一旗 on 5/7/16.
+//  Copyright © 2016 1tsuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
