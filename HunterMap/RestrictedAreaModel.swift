8e8ef2043c249fd5d4cdcc36d7509b4535e0dc3d
Itsuki.Sakitsu <itsuki.sakitsu@gmail.com>
2016-05-10 12:11:09 +0900

    fixed var name from dimension to boundingRectDimension

--- HunterMap/RestrictedAreaModel.swift
+++ HunterMap/RestrictedAreaModel.swift
@@ -10,60 +10,6 @@ import Foundation
 import MapKit
 import shapelib
 
-/**
- * dataSource : http://nlftp.mlit.go.jp/ksj/jpgis/datalist/KsjTmplt-A15.html
- */
-enum ShapeFiles: String {
-    case Hokkaido = "A15-09_01_WildlifePreserve"
-    case Aomori = "A15-09_02_WildlifePreserve"
-    case Iwate = "A15-09_03_WildlifePreserve"
-    case Miyagi = "A15-09_04_WildlifePreserve"
-    case Akita = "A15-09_05_WildlifePreserve"
-    case Yamagata = "A15-09_06_WildlifePreserve"
-    case Fukushima = "A15-09_07_WildlifePreserve"
-    case Ibaraki = "A15-09_08_WildlifePreserve"
-    case Tochigi = "A15-09_09_WildlifePreserve"
-    case Gumma = "A15-09_10_WildlifePreserve"
-    case Saitama = "A15-09_11_WildlifePreserve"
-    case Chiba = "A15-09_12_WildlifePreserve"
-    case Tokyo = "A15-09_13_WildlifePreserve"
-    case Kanagawa = "A15-09_14_WildlifePreserve"
-    case Nigata = "A15-09_15_WildlifePreserve"
-    case Toyama = "A15-09_16_WildlifePreserve"
-    case Ishikawa = "A15-09_17_WildlifePreserve"
-    case Fukui = "A15-09_18_WildlifePreserve"
-    case Yamanashi = "A15-09_19_WildlifePreserve"
-    case Nagano = "A15-09_20_WildlifePreserve"
-    case Gifu = "A15-09_21_WildlifePreserve"
-    case Shizuoka = "A15-09_22_WildlifePreserve"
-    case Aichi = "A15-09_23_WildlifePreserve"
-    case Mie = "A15-09_24_WildlifePreserve"
-    case Shiga = "A15-09_25_WildlifePreserve"
-    case Kyoto = "A15-09_26_WildlifePreserve"
-    case Osaka = "A15-09_27_WildlifePreserve"
-    case Hyogo = "A15-09_28_WildlifePreserve"
-    case Nara = "A15-09_29_WildlifePreserve"
-    case Wakayama = "A15-09_30_WildlifePreserve"
-    case Tottori = "A15-09_31_WildlifePreserve"
-    case Shimane = "A15-09_32_WildlifePreserve"
-    case Okayama = "A15-09_33_WildlifePreserve"
-    case Hiroshima = "A15-09_34_WildlifePreserve"
-    case Yamaguchi = "A15-09_35_WildlifePreserve"
-    case Tokushima = "A15-09_36_WildlifePreserve"
-    case Kagawa = "A15-09_37_WildlifePreserve"
-    case Ehime = "A15-09_38_WildlifePreserve"
-    case Kochi = "A15-09_39_WildlifePreserve"
-    case Fukuoka = "A15-09_40_WildlifePreserve"
-    case Saga = "A15-09_41_WildlifePreserve"
-    case Nagasaki = "A15-09_42_WildlifePreserve"
-    case Kumamoto = "A15-09_43_WildlifePreserve"
-    case Oita = "A15-09_44_WildlifePreserve"
-    case Miyazaki = "A15-09_45_WildlifePreserve"
-    case Kagoshima = "A15-09_46_WildlifePreserve"
-    case Okinawa = "A15-09_47_WildlifePreserve"
-    static let entries = [Hokkaido, Aomori, Iwate, Miyagi, Akita, Yamagata, Fukushima, Ibaraki, Tochigi, Gumma, Saitama, Chiba, Tokyo, Kanagawa, Nigata, Toyama, Ishikawa, Fukui, Yamanashi, Nagano, Gifu, Shizuoka, Aichi, Mie, Shiga, Kyoto, Osaka, Hyogo, Nara, Wakayama, Tottori, Shimane, Okayama, Hiroshima, Yamaguchi, Tokushima, Kagawa, Ehime, Kochi, Fukuoka, Saga, Nagasaki, Kumamoto, Oita, Miyazaki, Kagoshima, Okinawa]
-}
-
 enum AreaType: Int {
     case WildlifeProtectionArea = 1
     case SpecialProtectionArea = 2
@@ -75,11 +21,11 @@ enum AreaType: Int {
 struct RestrictedArea {
     // defined as variable for MKPolygon, basically its a let
     var coordinates: [CLLocationCoordinate2D]
+    let boundingRectDimension: Double
     let maxLat: Double
     let minLat: Double
     let maxLng: Double
     let minLng: Double
-    let dimention: Double
     let id: Int
     let pref: Int
     let areaType: Int
@@ -90,13 +36,57 @@ struct RestrictedArea {
 }
 
 class RestrictedAreaModel {
-    
-    // make class singleton
-    static var sharedInstance: RestrictedAreaModel = {
-        return RestrictedAreaModel()
-    }()
-    private init() {
-        reload()
+    // data sources
+    // http://nlftp.mlit.go.jp/ksj/jpgis/datalist/KsjTmplt-A15.html
+    enum ShapeFiles: String {
+        case Hokkaido = "A15-09_01_WildlifePreserve"
+        case Aomori = "A15-09_02_WildlifePreserve"
+        case Iwate = "A15-09_03_WildlifePreserve"
+        case Miyagi = "A15-09_04_WildlifePreserve"
+        case Akita = "A15-09_05_WildlifePreserve"
+        case Yamagata = "A15-09_06_WildlifePreserve"
+        case Fukushima = "A15-09_07_WildlifePreserve"
+        case Ibaraki = "A15-09_08_WildlifePreserve"
+        case Tochigi = "A15-09_09_WildlifePreserve"
+        case Gumma = "A15-09_10_WildlifePreserve"
+        case Saitama = "A15-09_11_WildlifePreserve"
+        case Chiba = "A15-09_12_WildlifePreserve"
+        case Tokyo = "A15-09_13_WildlifePreserve"
+        case Kanagawa = "A15-09_14_WildlifePreserve"
+        case Nigata = "A15-09_15_WildlifePreserve"
+        case Toyama = "A15-09_16_WildlifePreserve"
+        case Ishikawa = "A15-09_17_WildlifePreserve"
+        case Fukui = "A15-09_18_WildlifePreserve"
+        case Yamanashi = "A15-09_19_WildlifePreserve"
+        case Nagano = "A15-09_20_WildlifePreserve"
+        case Gifu = "A15-09_21_WildlifePreserve"
+        case Shizuoka = "A15-09_22_WildlifePreserve"
+        case Aichi = "A15-09_23_WildlifePreserve"
+        case Mie = "A15-09_24_WildlifePreserve"
+        case Shiga = "A15-09_25_WildlifePreserve"
+        case Kyoto = "A15-09_26_WildlifePreserve"
+        case Osaka = "A15-09_27_WildlifePreserve"
+        case Hyogo = "A15-09_28_WildlifePreserve"
+        case Nara = "A15-09_29_WildlifePreserve"
+        case Wakayama = "A15-09_30_WildlifePreserve"
+        case Tottori = "A15-09_31_WildlifePreserve"
+        case Shimane = "A15-09_32_WildlifePreserve"
+        case Okayama = "A15-09_33_WildlifePreserve"
+        case Hiroshima = "A15-09_34_WildlifePreserve"
+        case Yamaguchi = "A15-09_35_WildlifePreserve"
+        case Tokushima = "A15-09_36_WildlifePreserve"
+        case Kagawa = "A15-09_37_WildlifePreserve"
+        case Ehime = "A15-09_38_WildlifePreserve"
+        case Kochi = "A15-09_39_WildlifePreserve"
+        case Fukuoka = "A15-09_40_WildlifePreserve"
+        case Saga = "A15-09_41_WildlifePreserve"
+        case Nagasaki = "A15-09_42_WildlifePreserve"
+        case Kumamoto = "A15-09_43_WildlifePreserve"
+        case Oita = "A15-09_44_WildlifePreserve"
+        case Miyazaki = "A15-09_45_WildlifePreserve"
+        case Kagoshima = "A15-09_46_WildlifePreserve"
+        case Okinawa = "A15-09_47_WildlifePreserve"
+        static let entries = [Hokkaido, Aomori, Iwate, Miyagi, Akita, Yamagata, Fukushima, Ibaraki, Tochigi, Gumma, Saitama, Chiba, Tokyo, Kanagawa, Nigata, Toyama, Ishikawa, Fukui, Yamanashi, Nagano, Gifu, Shizuoka, Aichi, Mie, Shiga, Kyoto, Osaka, Hyogo, Nara, Wakayama, Tottori, Shimane, Okayama, Hiroshima, Yamaguchi, Tokushima, Kagawa, Ehime, Kochi, Fukuoka, Saga, Nagasaki, Kumamoto, Oita, Miyazaki, Kagoshima, Okinawa]
     }
     
     // fieldNames defined in DBF
@@ -108,8 +98,16 @@ class RestrictedAreaModel {
     let kFieldNameListedDate = "A15_006"
     let kFieldNameDelistedDate = "A15_007"
     
-    var restrictedAreas = [RestrictedArea]()
+    // make class singleton
+    static var sharedInstance: RestrictedAreaModel = {
+        return RestrictedAreaModel()
+    }()
+    private init() {
+        reload()
+    }
     
+    // cached data container
+    var restrictedAreas = [RestrictedArea]()
     
     func getAll(loadFromSource: Bool = false) -> [RestrictedArea] {
         if loadFromSource {
@@ -139,19 +137,18 @@ class RestrictedAreaModel {
         for shapeFile in ShapeFiles.entries {
             restrictedAreas.appendContentsOf(loadFromSource(shapeFile.rawValue))
         }
-        restrictedAreas.sortInPlace({ $0.0.dimention > $0.1.dimention })
+        restrictedAreas.sortInPlace({ $0.0.boundingRectDimension > $0.1.boundingRectDimension })
     }
     
     private func loadFromSource(resourceName: String) -> [RestrictedArea] {
         var areas = [RestrictedArea]()
-        
         func handler(args: ShapeFileHandlerArgs) -> Void {
             let area = RestrictedArea(coordinates: args.coordinates,
-                                      maxLat: args.maxLat,
-                                      minLat: args.minLat,
-                                      maxLng: args.maxLng,
-                                      minLng: args.minLng,
-                                      dimention:  args.dimension,
+                                      boundingRectDimension:  args.boundingRectDimension,
+                                      maxLat: args.shpObject.dfYMax,
+                                      minLat: args.shpObject.dfYMin,
+                                      maxLng: args.shpObject.dfXMax,
+                                      minLng: args.shpObject.dfXMin,
                                       id: getIntFieldValue(kFieldNameId, dbf: args.dbf, shpObject: args.shpObject),
                                       pref: getIntFieldValue(kFieldNamePref, dbf: args.dbf, shpObject: args.shpObject),
                                       areaType: getIntFieldValue(kFieldNameAreaType, dbf: args.dbf, shpObject: args.shpObject),
@@ -166,14 +163,7 @@ class RestrictedAreaModel {
         ShapeFileUtil.loadShapeFile(path, handler: handler)
         
         return areas
-    }
-    
-    private func isLarger(a: RestrictedArea, b: RestrictedArea) -> Bool {
-        let sizeA = (a.maxLat - a.minLat) * (a.maxLng - a.minLng)
-        let sizeB = (b.maxLat - b.minLat) * (b.maxLng - b.minLng)
-        return sizeA > sizeB
-    }
-    
+    }    
     private func getStringFieldValue(fieldName: String, dbf: DBFHandle, shpObject: SHPObject) -> String? {
         let fieldIndex = DBFGetFieldIndex(dbf, fieldName)
         return String.fromCString(DBFReadStringAttribute(dbf, shpObject.nShapeId, fieldIndex))
--- HunterMap/ShapeFileUtil.swift
+++ HunterMap/ShapeFileUtil.swift
@@ -12,27 +12,28 @@ import shapelib
 
 struct ShapeFileHandlerArgs {
     let coordinates: [CLLocationCoordinate2D]
-    let maxLat: Double
-    let minLat: Double
-    let maxLng: Double
-    let minLng: Double
-    let dimension: Double
+    let boundingRectDimension: Double
     let shpObject: SHPObject
     let dbf: DBFHandle
 }
 
 class ShapeFileUtil {
     
-    
     static let kShapeFileExtension = "shp"
     
-    // remove extention from resource
+    /**
+     remove extention from resource
+     make sure that shape files are already added as resource
+     */
     static func getResourcePathForShapeLib(resourceName: String) -> String {
         let shpFilePath = NSBundle.mainBundle().pathForResource(resourceName, ofType: kShapeFileExtension)
         return (shpFilePath! as NSString).stringByAppendingPathComponent("")
     }
     
-    // load shape file with handler method
+    /**
+     load shape file with result handler
+     arg "path" should not contain extension, use getResourcePathForShapeLib for it
+     */
     static func loadShapeFile(path: String, handler: ((args: ShapeFileHandlerArgs)->Void)?) {
         // open dbf
         let dbf = DBFOpen(path, "rb")
@@ -41,21 +42,21 @@ class ShapeFileUtil {
         // open shp
         let shp = SHPOpen(path, "rb")
         defer { SHPClose(shp) }
-        var pnEntities: Int32 = 0
-        SHPGetInfo(shp, &pnEntities, nil, nil, nil)
         
         // extract all SHPObjects
+        var pnEntities: Int32 = 0
+        SHPGetInfo(shp, &pnEntities, nil, nil, nil)
         for i in 0..<pnEntities {
             // read SHPObject
             let shpObjectPointer = SHPReadObject(shp, i)
             defer { SHPDestroyObject(shpObjectPointer) }
-            
             let shpObject = shpObjectPointer.memory
+            
+            // SHPOBject contains several parts
             let numParts = Int(shpObject.nParts)
             let totalVertexCount = Int(shpObject.nVertices)
-            
-            // extract all Vertices
             for j in 0..<numParts {
+                // extract vertices belonging to part
                 let startVertex = Int(shpObject.panPartStart[j])
                 let endIndex = (j == numParts-1) ? totalVertexCount : Int(shpObject.panPartStart[j+1])
                 var coordinates = [CLLocationCoordinate2D]()
@@ -64,22 +65,20 @@ class ShapeFileUtil {
                     coordinates.append(coord)
                 }
                 
-                
+                // handle entity
                 handler?(args: ShapeFileHandlerArgs(
                     coordinates: coordinates,
-                    maxLat: shpObject.dfYMax,
-                    minLat: shpObject.dfYMin,
-                    maxLng: shpObject.dfXMax,
-                    minLng: shpObject.dfXMin,
-                    dimension: getDimention(shpObject),
+                    boundingRectDimension: getBoundingRectDimention(shpObject),
                     shpObject: shpObject,
                     dbf: dbf))
             }
-            
         }
     }
     
-    static func getDimention(shpObject: SHPObject) -> Double {
+    /**
+     Calculate dimension of bounding rect
+     */
+    private static func getBoundingRectDimention(shpObject: SHPObject) -> Double {
         return fabs(shpObject.dfXMax - shpObject.dfXMin) * fabs(shpObject.dfYMax - shpObject.dfYMin)
     }
 }
